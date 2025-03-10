238f69760b3cfc52b62864707e2c0af3fc66a8fd
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-03-10 21:21:23 +0100

    Add vermutinho

--- Meta Django/myproject/myapp/urls.py
+++ Meta Django/myproject/myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home),
+    path('dishes/<str:dish>', views.menuitems),
+    path('drinks/<str:drink_name>', views.drinks, name='drinks'),
 ]
--- Meta Django/myproject/myapp/views.py
+++ Meta Django/myproject/myapp/views.py
@@ -18,3 +18,26 @@ def home(request):
         return response
     """
     return HttpResponse(msg, content_type='text/html', charset='utf-8')
+
+def menuitems(request, dish):
+    items = {
+        'pasta': 'Pasta is a type of noodle',
+        'falafel': 'Falafel are deep fried',
+        'cheesecake': 'Is a type of dessert',
+    }
+
+    description = items [dish]
+
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment',
+        'vermutinho': 'With olives and orange'
+    }
+
+    choice_of_drink = drink[drink_name]
+
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)

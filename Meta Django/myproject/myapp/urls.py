30c12f282dc497fd044516a8b36b9196aac01b0e
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-03-11 15:03:57 +0100

    Creating urls and mapping to views

--- Meta Django/myproject/myapp/urls.py
+++ Meta Django/myproject/myapp/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('dishes/<str:dish>', views.menuitems),
-    path('drinks/<str:drink_name>', views.drinks, name='drinks'),
+    path('', views.home, name="home"),
+    path('aboutus/', views.about, name='abaout'),
+    path('menu/', views.menu, name="menu"),
+    path('book/', views.book, name="book")
 ]
+
+
+# urlpatterns = [
+#     path('home/', views.home),
+#     path('dishes/<str:dish>', views.menuitems),
+#     path('drinks/<str:drink_name>', views.drinks, name='drinks'),
+# ]
--- Meta Django/myproject/myapp/views.py
+++ Meta Django/myproject/myapp/views.py
@@ -4,40 +4,54 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    path = request.path
-    scheme = request.scheme
-    method = request.method
-    address = request.META['REMOTE_ADDR']
+    return HttpResponse("Welcome to Little Lemon!")
 
-    msg = f"""<br>
-        <br>Path: {path}
-        <br>Address: {address}
-        <br>Scheme: {scheme}
-        <br>Method: {method}
-        response = HttpResponse("This work")
-        return response
-    """
-    return HttpResponse(msg, content_type='text/html', charset='utf-8')
+def about(request):
+    return HttpResponse("About us")
 
-def menuitems(request, dish):
-    items = {
-        'pasta': 'Pasta is a type of noodle',
-        'falafel': 'Falafel are deep fried',
-        'cheesecake': 'Is a type of dessert',
-    }
+def menu(request):
+    return HttpResponse("Menu")
 
-    description = items [dish]
+def book(request):
+    return HttpResponse('Make a booking')
 
-    return HttpResponse(f"<h2> {dish} </h2>" + description)
 
-def drinks(request, drink_name):
-    drink = {
-        'mocha': 'type of coffee',
-        'tea': 'type of beverage',
-        'lemonade': 'type of refreshment',
-        'vermutinho': 'With olives and orange'
-    }
 
-    choice_of_drink = drink[drink_name]
+# def home(request):
+#     path = request.path
+#     scheme = request.scheme
+#     method = request.method
+#     address = request.META['REMOTE_ADDR']
 
-    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
+#     msg = f"""<br>
+#         <br>Path: {path}
+#         <br>Address: {address}
+#         <br>Scheme: {scheme}
+#         <br>Method: {method}
+#         response = HttpResponse("This work")
+#         return response
+#     """
+#     return HttpResponse(msg, content_type='text/html', charset='utf-8')
+
+# def menuitems(request, dish):
+#     items = {
+#         'pasta': 'Pasta is a type of noodle',
+#         'falafel': 'Falafel are deep fried',
+#         'cheesecake': 'Is a type of dessert',
+#     }
+
+#     description = items [dish]
+
+#     return HttpResponse(f"<h2> {dish} </h2>" + description)
+
+# def drinks(request, drink_name):
+#     drink = {
+#         'mocha': 'type of coffee',
+#         'tea': 'type of beverage',
+#         'lemonade': 'type of refreshment',
+#         'vermutinho': 'With olives and orange'
+#     }
+
+#     choice_of_drink = drink[drink_name]
+
+#     return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)

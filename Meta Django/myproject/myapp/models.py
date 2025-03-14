98fadc2e263f8f753f41850637ff400ea5a9ba8c
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-03-14 01:43:03 +0100

    ORM

--- Meta Django/myproject/myapp/models.py
+++ Meta Django/myproject/myapp/models.py
@@ -15,3 +15,13 @@ class Menu(models.Model):
         on_delete=models.PROTECT,
         default=None,
         related_name="category_name")
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE,
+                                 related_name='Vehicle'
+                                 )

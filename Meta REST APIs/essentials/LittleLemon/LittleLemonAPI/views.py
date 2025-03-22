7b3836bbecaae670b8f9ccc27e179cd65f7187fd
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-03-22 22:02:17 +0100

    Serializer class

---
+++ Meta REST APIs/essentials/LittleLemon/LittleLemonAPI/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
+# Create your views here.
+
+@api_view(['GET', "POST"])
+def menu_items(request):
+    items = MenuItem.objects.all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+@api_view()
+def single_view(request, id):
+    item = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(item)#Many = true not required for single options
+    return Response(serialized_item.data)

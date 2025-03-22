785e41a2f45909b9e9cb6ab85cfd5460d7f7fa6c
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-03-23 00:07:44 +0100

    Deserialization and validation

--- Meta REST APIs/essentials/LittleLemon/LittleLemonAPI/serializers.py
+++ Meta REST APIs/essentials/LittleLemon/LittleLemonAPI/serializers.py
@@ -17,10 +17,11 @@ class CategorySerializer (serializers.ModelSerializer):
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory') # Changing the Iventory name to stock only user display.
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax') # How to add new field only user display
-    category = CategorySerializer()
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category']
+        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
 
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)

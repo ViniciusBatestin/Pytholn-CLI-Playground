ef7aee3fa7f64b74708dbd45405b44e27aed9043
ViniciusBatestin <viniciusbatestin@gmail.com>
2025-04-04 21:36:14 +0200

    lab initial

--- Meta Full-Stack/C7M5L1-6 Lab Initial/restaurant/views.py
+++ Meta Full-Stack/C7M5L1-6 Lab Initial/restaurant/views.py
@@ -6,7 +6,7 @@ from django.core import serializers
 from .models import Booking
 from datetime import datetime
 import json
-# from .forms import BookingForm
+from .forms import BookingForm
 
 def home(request):
     return render(request, 'index.html')
@@ -14,18 +14,22 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# def book(request):
-#     form = BookingForm()
-#     if request.method == 'POST':
-#         form = BookingForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#     context = {'form':form}
-#     return render(request, 'book.html', context)
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
 
 # Add code for the bookings() view
 
-
+def bookings(request):
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.filter(reservation_date = date)
+    booking_json= serializers.serialize('json', bookings)
+    return render(request, 'bookings.html', {'bookings': booking_json})
 
 def menu(request):
     menu_data = Menu.objects.all()
@@ -33,9 +37,9 @@ def menu(request):
     return render(request, 'menu.html', {"menu": main_data})
 
 
-def display_menu_item(request, pk=None): 
-    if pk: 
-        menu_item = Menu.objects.get(pk=pk) 
-    else: 
-        menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
